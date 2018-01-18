68264af726250f2846029936a2fb2982f7128af8
Jinhuyk Park <pjh08190819@naver.com>
2018-01-18 11:16:13 +0900

    test

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/JinHyukParkk/DownloadTile/downloads"
@@ -11,15 +11,16 @@ import (
 func main() {
 	t0 := time.Now()
 	//argument
-	site := os.Args[1]
-	level := os.Args[2]
-	firstD := os.Args[3]
-	secondD := os.Args[4]
 
-	if site == "naver" {
-		downloads.ConstructNaverUrl(level, firstD, secondD)
-	} else if site == "daum" {
-		downloads.ConstructDaumUrl(level, firstD, secondD)
+	site := flag.String("site", "naver", "Site Name")
+	level := flag.String("lv", "1", "Tile Level")
+	firstD := flag.String("x", "1", "Tile x Location")
+	secondD := flag.String("y", "1", "Tile y Location")
+	flag.Parse()
+	if *site == "naver" {
+		downloads.ConstructNaverUrl(*level, *firstD, *secondD)
+	} else if *site == "daum" {
+		downloads.ConstructDaumUrl(*level, *firstD, *secondD)
 	}
 
 	t1 := time.Now()
